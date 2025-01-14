489d6b4883ff96e8afb6baabfc0714a967d1f5f2
amspsingh04 <sp.singh.150604@gmail.com>
2025-01-15 03:42:49 +0530

    first commit

---
+++ example_des.py
@@ -0,0 +1,8 @@
+from des import DES
+
+# Example usage
+plaintext = "0123456789ABCDEF"
+key = "133457799BBCDFF1"
+encrypted = DES.des_encrypt(plaintext, key)
+print("Encrypted message:", encrypted)
+## output will be 85E813540F0AB405
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Functions available so far
+
+### DES Encryption
+A Python implementation of the DES encryption algorithm.
+
+## Installation
+
+```bash
+pip install des-encryption
